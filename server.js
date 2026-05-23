8126f5f44851064050a9679483dc7198f3110b17
esmamian <esmamian@unicauca.edu.co>
2026-05-22 19:43:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -367,7 +367,7 @@ app.post("/guardar-encabezado", auth, async (req, res) => {
         lineas_investigacion
       )
       VALUES (
-        $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15
+        $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16
       )`,
       [
         usuario,
